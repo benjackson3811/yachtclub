2b0e8bffc94b8bd4eed567dadc049145967286ea
benjackson3811 <benjackson3811@googlemail.com>
2023-10-06 11:31:41 +0000

    created followers app, models.py + serializers

---
+++ followers/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Follower(models.Model):
+    """
+    Follower model, linked to 'owner' and 'followed'.
+    'owner' = is the user whom is following a User.
+    'followed' = is the User that is followed by 'owner'.
+    """
+    owner = models.ForeignKey(User, related_name='following', on_delete=models.CASCADE)
+    followed = models.ForeignKey(User, related_name='followed', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['-created_at']
+        unique_together = ['owner', 'followed']
+        
+    def __str__(self):
+        	     return f'{self.owner} {self.followed}'
--- yacht_club_api/settings.py
+++ yacht_club_api/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'profiles',
     'comments',
     'trips',
+    'followers',
 ]
 
 MIDDLEWARE = [
