e56cf969b930f0f6080d0260021e94111c305ec8
benjackson3811 <benjackson3811@googlemail.com>
2023-10-14 11:45:23 +0000

    Fix - remove trip serializer boilerplate and update

--- trips/serializers.py
+++ trips/serializers.py
@@ -4,39 +4,17 @@ from likes.models import Like
 
 
 class TripSerializer(serializers.ModelSerializer):
-    owner = serializers.ReadOnlyField(source='owner.username')
-    is_owner = serializers.SerializerMethodField()
-    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
-    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
-    like_id = serializers.SerializerMethodField()
-    likes_count = serializers.ReadOnlyField()
-    comments_count = serializers.ReadOnlyField()
-    
-    def validate_image(self, value):
-        if value.size > 2 * 1024 * 1024:
-            raise serializers.ValidationError('Image size larger than 2MB!')
-        if value.image.height > 4096:
-            raise serializers.ValidationError(
-                'Image height is larger than 4096px!'
-                )
-        if value.image.width > 4096:
-            raise serializers.ValidationError(
-                'Image width is larger than 4096px!'
-                )
-            return value
+    user = serializers.ReadOnlyField(source='user.username')
+    is_user = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='user.profile.id')
+    avatar = serializers.ReadOnlyField(source='user.profile.avatar.url')
             
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
-        
-    def get_like_id(self, obj):
-        user = self.context['request'].user
-        if user.is_authenticated:
-            like = Like.objects.filter(
-                owner=user, trip=obj
-                ).first()
-            return like.id if like else None
-        return None
 
     class Meta:
         model = Trip
+        fields = ['id', 'user', 'is_user', 'profile_id', 
+        'avatar', 'created_at', 'updated_at', 'trip_title',
+        'description', 'image', 'category', 'trip_categories' ]
