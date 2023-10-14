28631f1f7c0dc32c419820f9511221f68ca17850
benjackson3811 <benjackson3811@googlemail.com>
2023-10-14 13:13:10 +0000

    Add post listview to trip app

--- trips/serializers.py
+++ trips/serializers.py
@@ -1,14 +1,13 @@
 from rest_framework import serializers
 from trips.models import Trip
 
+
 class TripSerializer(serializers.ModelSerializer):
     user = serializers.ReadOnlyField(source='user.username')
     is_user = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='user.profile.id')
     avatar = serializers.ReadOnlyField(source='user.profile.avatar.url')
-    """
-    validation for size of image 2mb, width and length of picture for 4096px 
-    """
+
     def validate_image(self, value):
         if value.size > 1024 * 1024 *2:
             raise serializers.ValidationError(
@@ -23,10 +22,10 @@ class TripSerializer(serializers.ModelSerializer):
                 'Image height larger than 4096px'
         )
         return value
-           
+      
     def get_is_user(self, obj):
         request = self.context['request']
-        return request.user == obj.owner
+        return request.user == obj.user
 
     class Meta:
         model = Trip
--- trips/views.py
+++ trips/views.py
@@ -1,4 +1,4 @@
-from rest_framework import status
+from rest_framework import status, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Trip
@@ -10,10 +10,27 @@ class TripList(APIView):
     List Trip or create a trip if logged in
     The perform_create method associates the trip with the logged in user.
     """
+    serializer_class = TripSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly
+    ]
     def get(self, request):
         trips = Trip.objects.all()
         serializer = TripSerializer(
             trips, many=True, context={'request': request}
         )
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = TripSerializer(
+            data=request.data, context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(
+                serializer.data, status=status.HTTP_201_CREATED
+                )
+        return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
 
