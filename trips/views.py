bddb24684bdb45ef49f46425b60b983561f2b781
benjackson3811 <benjackson3811@googlemail.com>
2023-10-14 18:16:36 +0000

    Fix - trips views.py refactored

--- trips/views.py
+++ trips/views.py
@@ -1,74 +1,30 @@
-from django.http import Http404
-from rest_framework import status, permissions
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import generics, permissions
 from .models import Trip
 from .serializers import TripSerializer
 from yacht_club_api.permissions import IsOwnerOrReadOnly
 
 
-class TripList(APIView):
+class TripList(generics.ListCreateAPIView):
     """
-    Logged in - List Trips / able to create a trip
-    The perform_create method associates the trip with the logged in user.
+    Class ability:
+    Create posts/ list created trips
+    trips are associates the trip with the logged in user.
     """
     serializer_class = TripSerializer
-    permission_classes = [
-        permissions.IsAuthenticatedOrReadOnly
-    ]
-    def get(self, request):
-        trips = Trip.objects.all()
-        serializer = TripSerializer(
-            trips, many=True, context={'request': request}
-        )
-        return Response(serializer.data)
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Trip.objects.all()
 
-    def post(self, request):
-        serializer = TripSerializer(
-            data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save(user=request.user)
-            return Response(
-                serializer.data, status=status.HTTP_201_CREATED
-                )
-        return Response(
-            serializer.errors, status=status.HTTP_400_BAD_REQUEST
-        )
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
-class TripDetail(APIView):
+
+class TripDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Class ability:
+    Retrieve a post,
+    Edit,
+    Delete it if you own it.
+    """
     serializer_class = TripSerializer
     permission_classes = [IsOwnerOrReadOnly]
-
-    def get_object(self, pk):
-        try: 
-            trip = Trip.objects.get(pk=pk)
-            self.check_object_permissions(self.request, trip)
-            return trip
-        except Trip.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk):
-        trip = self.get_object(pk)
-        serializer = TripSerializer(
-            trip, context={'request': request})
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        trip = self.get_object(pk)
-        serializer = TripSerializer(
-            trip, data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(
-            serializer.errors, status=status.HTTP_400_BAD_REQUEST
-        )
-
-    def delete(self, request, pk):
-        trip = self.get_object(pk)
-        trip.delete()
-        return Response(
-            status=status.HTTP_204_NO_CONTENT
-        )
+    queryset = Trip.objects.all()
