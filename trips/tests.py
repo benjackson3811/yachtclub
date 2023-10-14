e7376b487e3c495b21389c8d961ac52440016897
benjackson3811 <benjackson3811@googlemail.com>
2023-10-14 14:14:02 +0000

    Add, Trip app api test case

--- trips/tests.py
+++ trips/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Trip
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+class TripListViewTests(APITestCase):
+    def setUp(self):
+        User.objects.create_user(username='ben', password='pass')
+
+    def test_can_list_trips(self):
+        ben = User.objects.get(username='ben')
+        Trip.objects.create(user=ben, trip_title='a trip')
+        response = self.client.get('/trips/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        print(response.data)
+        print(len(response.data))
