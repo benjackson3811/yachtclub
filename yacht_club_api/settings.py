104cc1eb64604c4d04b56311329b1f6fb2a36832
benjackson3811 <benjackson3811@googlemail.com>
2023-10-21 14:37:39 +0000

    update, settings.py - to remove cors

--- yacht_club_api/settings.py
+++ yacht_club_api/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
-import re
 
 if os.path.exists('env.py'):
     import env
@@ -59,12 +58,12 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEV' in os.environ
+DEBUG = 'DEBUG' in os.environ
 
 ALLOWED_HOSTS = [
+    os.environ()'ALLOWED_HOST'),
    'localhost',
-   'yacht_club_api.herokuapp.com', 
-   'yacht-club-api-e8eb7fe2020a.herokuapp.com'
+   'yacht_club_api.herokuapp.com',
 ]
 
 # Application definition
@@ -106,14 +105,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
-if 'CLIENT_ORIGIN_DEV' in os.environ:
-    extracted_url = re.match(
-        r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE
-    ).group(0)
-    CORS_ALLOWED_ORIGIN_REGEXES = [
-        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
-    ]
 
 if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
