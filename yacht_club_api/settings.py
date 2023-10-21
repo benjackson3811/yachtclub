46e072b656378f72079d9062ea1e470b21b891db
benjackson3811 <benjackson3811@googlemail.com>
2023-10-21 15:16:03 +0000

    Prepare template teansfer

--- yacht_club_api/settings.py
+++ yacht_club_api/settings.py
@@ -61,7 +61,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = 'DEBUG' in os.environ
 
 ALLOWED_HOSTS = [
-    os.environ()'ALLOWED_HOST'),
+    os.environ.get('ALLOWED_HOST'),
    'localhost',
    'yacht_club_api.herokuapp.com',
 ]
