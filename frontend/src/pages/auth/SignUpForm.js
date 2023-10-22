e559dcae118c0701188efc791972c869748548b8
benjackson3811 <benjackson3811@googlemail.com>
2023-10-22 19:11:27 +0000

    Add basic signup form

--- frontend/src/pages/auth/SignUpForm.js
+++ frontend/src/pages/auth/SignUpForm.js
@@ -13,8 +13,41 @@ const SignUpForm = () => {
       <Col className="my-auto py-2 p-md-2" md={6}>
         <Container className={`${appStyles.Content} p-4 `}>
           <h1 className={styles.Header}>sign up</h1>
+            <Form>
+              <Form.Group controlId="username">
+                <Form.Label className="d-none">username</Form.Label>
+                <Form.Control
+                className={styles.Input}
+                type="text" 
+                placeholder="username" 
+                name="username" />
+              </Form.Group>
 
-            {/* add your form here */}
+              <Form.Group controlId="password1">
+                <Form.Label className="d-none" >Password</Form.Label>
+                <Form.Control
+                className={styles.Input}
+                type="password" 
+                placeholder="password1" 
+                name="password1" 
+                />
+              </Form.Group>
+
+              <Form.Group controlId="password2">
+                <Form.Label className="d-none">Confirm password</Form.Label>
+                <Form.Control
+                className={styles.Input}
+                type="password" 
+                placeholder="Confirm password" 
+                name="password2" 
+                />
+              </Form.Group>
+
+              <Button className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`} 
+              type="submit">
+                Sign up
+              </Button>
+            </Form>
 
         </Container>
         <Container className={`mt-3 ${appStyles.Content}`}>
