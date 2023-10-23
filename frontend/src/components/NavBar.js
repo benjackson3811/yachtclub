e88ee27a2643e86da4f9fa047b308de2dee69f91
benjackson3811 <benjackson3811@googlemail.com>
2023-10-23 20:37:06 +0000

    Fix - TuFix - fix to allow app deployment into heroku

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -8,6 +8,16 @@ import { useCurrentUser } from '../contexts/CurrentUserContext';
 const NavBar = () => {
   const currentUser = useCurrentUser();
 
+  const addTrip = (
+    <NavLink 
+    className={styles.NavLink} 
+    activeClassName={styles.Active} 
+    to="/trips/create">
+
+      <i className="fas fa-sign-in-alt"></i>Sign in
+  </NavLink>
+  )
+
   const loggedInIcons = <>{currentUser?.username}</>;
   const loggedOutIcons =  (
     <>
