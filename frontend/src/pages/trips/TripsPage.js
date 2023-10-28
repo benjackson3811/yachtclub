6c8b01ca7d7272abf90a162bacb8adf1045cd945
benjackson3811 <benjackson3811@googlemail.com>
2023-10-28 15:54:33 +0000

    Add, display and filter ternerary to TripsPage

--- frontend/src/pages/trips/TripsPage.js
+++ frontend/src/pages/trips/TripsPage.js
@@ -5,10 +5,15 @@ import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 
+import Trip from "./Trip";
+import Asset from "../../components/Asset";
+
 import appStyles from "../../App.module.css";
 import styles from "../../styles/TripsPage.module.css";
 import { useLocation } from "react-router-dom";
 import { axiosReq } from "../../api/axiosDefaults";
+
+import NoResults from "../../assets/no-results.png";
 
 function TripsPage({ message, filter = ""}) {
     const [trips, setTrips] = useState({ results: [] });
@@ -34,7 +39,23 @@ function TripsPage({ message, filter = ""}) {
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles mobile</p>
-        <p>List of trips here</p>
+        {hasLoaded ? (
+          <>
+            {trips.results.length ? (
+              trips.results.map((trip) => (
+                <Trip key={trip.id} {...trip} setTrips={setTrips} />
+              ))
+            ) : (
+              <Container className={appStyles.Content}>
+                <Asset src={NoResults} message={message} />
+              </Container>
+            )}
+          </>
+        ) : (
+          <Container className={appStyles.Content}>
+            <Asset spinner />
+          </Container>
+        )}
       </Col>
       <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
         <p>Popular profiles for desktop</p>
