4dc66587b56d0973666411d4e3707a37f55e0147
benjackson3811 <benjackson3811@googlemail.com>
2023-10-29 13:40:44 +0000

    Add infinite scroll to comments

--- frontend/src/pages/trips/TripPage.js
+++ frontend/src/pages/trips/TripPage.js
@@ -12,6 +12,9 @@ import Comment from "../comments/Comment";
 
 import CommentCreateForm from "../comments/CommentCreateForm";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import InfiniteScroll from "react-infinite-scroll-component";
+import Asset from "../../components/Asset";
+import { fetchMoreData } from "../../utils/utils";
 
 function TripPage() {
   const { id } = useParams();
@@ -55,14 +58,20 @@ function TripPage() {
             "Comments"
           ) : null}
           {comments.results.length ? (
-            comments.results.map((comment) => (
-              <Comment
-                key={comment.id}
-                {...comment}
-                setTrip={setTrip}
-                setComments={setComments}
-             />
-            ))
+            <InfiniteScroll
+              children={comments.results.map((comment) => (
+                <Comment
+                  key={comment.id}
+                  {...comment}
+                  setTrip={setTrip}
+                  setComments={setComments}
+                />
+              ))}
+              dataLength={comments.results.length}
+              loader={<Asset spinner />}
+              hasMore={!!comments.next}
+              next={() => fetchMoreData(comments, setComments)}
+            />
           ) : currentUser ? (
             <span>No comments yet, be the first to comment!</span>
           ) : (
