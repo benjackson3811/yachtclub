7809bfbdafe9530641e198a3af9cdaa92698b221
benjackson3811 <benjackson3811@googlemail.com>
2023-10-29 19:54:39 +0000

    Create, storage, timestamp functions in utils

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -41,3 +41,16 @@ export const unfollowHelper = (profile, clickedProfile) => {
     ? { ...profile, following_count: profile.following_count - 1 }
     : profile;
 }
+
+  export const setTokenTimestamp = (data) => {
+    const refreshTokenTimestamp = jwtDecode(data?.refresh_token).exp;
+    localStorage.setItem("refreshTokenTimestamp", refreshTokenTimestamp);
+  };
+
+  export const shouldRefreshToken = () => {
+    return !!localStorage.getItem("refreshTokenTimestamp");
+  };
+
+  export const removeTokenTimestamp = () => {
+    localStorage.removeItem("refreshTokenTimestamp");
+  };
