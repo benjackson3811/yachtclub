1b42bb9487f5c68e29a238dd2793b3486362b831
benjackson3811 <benjackson3811@googlemail.com>
2023-10-29 14:55:43 +0000

    Add, popularProfiles data object defined

--- frontend/src/pages/trips/TripsPage.js
+++ frontend/src/pages/trips/TripsPage.js
@@ -16,6 +16,7 @@ import { axiosReq } from "../../api/axiosDefaults";
 import NoResults from "../../assets/no-results.png";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { fetchMoreData } from "../../utils/utils";
+import PopularProfiles from "../profiles/PopularProfiles";
 
 function TripsPage({ message, filter = "" }) {
   const [trips, setTrips] = useState({ results: [] });
@@ -87,7 +88,7 @@ function TripsPage({ message, filter = "" }) {
         )}
       </Col>
       <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
-        <p>Popular profiles for desktop</p>
+        <PopularProfiles />
       </Col>
     </Row>
   );
